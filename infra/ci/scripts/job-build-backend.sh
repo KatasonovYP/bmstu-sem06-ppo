c9e308e4e4c4f422e509c045d7281ed5d6fe992f
katasonovyp <katasonov-yury@yandex.ru>
2025-10-31 05:55:48 +0300

    refactor infra

--- infra/ci/scripts/job-build-backend.sh
+++ infra/ci/scripts/job-build-backend.sh
@@ -2,7 +2,7 @@
 
 cd ./backend || exit 0
 
-cargo +nightly build --release --all-targets --timings
+RUSTFLAGS="-Z threads=16" cargo +nightly build --release --all-targets --timings
 
 mkdir -p ./bin
 
