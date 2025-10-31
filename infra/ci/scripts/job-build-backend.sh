2d5c4f03c1004705e9a3fc2d0bdf741ac93d8377
katasonovyp <katasonov-yury@yandex.ru>
2025-10-31 03:51:37 +0300

    refactor infra

--- infra/ci/scripts/job-build-backend.sh
+++ infra/ci/scripts/job-build-backend.sh
@@ -23,14 +23,14 @@ mkdir -p ./allure-results
 
 TEST_FILE=./target/nextest/default/junit.xml
 
-cargo nextest run --release --nff --no-capture --lib || true
+cargo nextest run -q --release --nff --no-capture --lib || true
 xmlstarlet ed -L -P  -s '//testcase' -t elem -n skipped -v '' -d '//failure' ${TEST_FILE}
 mv ${TEST_FILE} ./allure-results/unit.xml
 
-cargo nextest run --release --nff --no-capture -p test_integration || true
+cargo nextest run -q --release --nff --no-capture -p test_integration || true
 xmlstarlet ed -L -P  -s '//testcase' -t elem -n skipped -v '' -d '//failure' ${TEST_FILE}
 mv ${TEST_FILE} ./allure-results/integration.xml
 
-cargo nextest run --release --nff --no-capture -p test_e2e || true
+cargo nextest run -q --release --nff --no-capture -p test_e2e || true
 xmlstarlet ed -L -P  -s '//testcase' -t elem -n skipped -v '' -d '//failure' ${TEST_FILE}
 mv ${TEST_FILE} ./allure-results/e2e.xml
